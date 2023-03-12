251422f65751b1d0a2b4404128f7992f54270654
Vladimir Simanovskii <v.simanovsky@gmail.com>
2023-03-12 21:21:52 +0700

    adaptive

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ import { like } from "./modules/like.js";
 import { createComment } from "./modules/createComment.js";
 import { getFormDate} from "./modules/getFormDate.js";
 import { autoresize } from "./modules/textarea.js";
+import { sidebarToggle } from "./mobilenav.js";
 
 flsFunctions.isWebp();
 
 let commentText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, similique sed natus voluptatibus maxime alias impedit molestias magni sint soluta ducimus omnis beatae veritatis! Ducimus eum libero quibusdam eos temporibus doloribus, neque explicabo quis, porro voluptate tempore quaerat voluptatum magni ea corrupti minima. Distinctio, sit exercitationem ipsum placeat nesciunt odit.';
 let newComment = document.getElementById('comments_container');
 newComment.append(createComment('Anon', commentText, '05.03.2023, 12:15'));
+sidebarToggle();
 like();
 remove();
 getFormDate();
